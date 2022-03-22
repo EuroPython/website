9f0dcfd348673e00290714abee20a12161e72181
Patrick Arminio <patrick.arminio@gmail.com>
2022-03-22 18:02:08 +0000

    Fix spacing

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: "/cfp",
-        destination: "https://program.europython.eu/europython-2022/cfp ",
+        destination: "https://program.europython.eu/europython-2022/cfp",
         permanent: true,
       },
     ];
