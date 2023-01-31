935a754d66d45b71a4a034ec04f3df0166c63748
Patrick Arminio <patrick.arminio@gmail.com>
2023-01-31 12:04:26 +0000

    Enable vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/main.css";
 import type { AppProps } from "next/app";
 import { MDXProvider } from "@mdx-js/react";
 import { components } from "../components/mdx";
+import { Analytics } from "@vercel/analytics/react";
 
 import dynamic from "next/dynamic";
 
@@ -14,6 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <MDXProvider components={components}>
       <Tweaks />
       <Component {...pageProps} />
+      <Analytics />
     </MDXProvider>
   );
 }
