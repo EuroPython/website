be15b2d2530a4e43f8f6073d8d863989c06decd8
Patrick Arminio <patrick.arminio@gmail.com>
2023-01-31 11:53:16 +0000

    Update script

--- components/meta.tsx
+++ components/meta.tsx
@@ -55,7 +55,7 @@ export const Meta = ({
       <script
         defer
         data-domain="ep2023.europython.eu"
-        src="https://plausible.io/js/plausible.js"
+        src="https://plausible.io/js/script.js"
       ></script>
     </Head>
   );
