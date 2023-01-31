1ab704d6a9515bea4177ef60c8bfd9ebdefc7b80
Patrick Arminio <patrick.arminio@gmail.com>
2023-01-31 16:30:03 +0000

    Remove vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,6 @@ import "../styles/main.css";
 import type { AppProps } from "next/app";
 import { MDXProvider } from "@mdx-js/react";
 import { components } from "../components/mdx";
-import { Analytics } from "@vercel/analytics/react";
 
 import dynamic from "next/dynamic";
 
@@ -15,7 +14,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     <MDXProvider components={components}>
       <Tweaks />
       <Component {...pageProps} />
-      <Analytics />
     </MDXProvider>
   );
 }
