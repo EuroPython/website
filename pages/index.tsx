3ee32d16061366e2bc10cf11232f0edacdb15991
Patrick Arminio <patrick.arminio@gmail.com>
2023-02-01 10:55:43 +0000

    Add social card in home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,8 @@ import { Hero } from "../components/hero";
 import Head from "next/head";
 
 export default function IndexPage() {
+  const imageUrl = "https://ep2023.europython.eu/social-cards/default.png";
+
   return (
     <>
       <Head>
@@ -33,11 +35,13 @@ export default function IndexPage() {
         <meta property="og:url" content="https://ep2023.europython.eu" />
         <meta property="og:title" content="EuroPython 2023 | July 17th-23rd 2023 | Prague, Czech Republic & Remote" />
         <meta property="og:description" content="" />
+        <meta property="og:image" content={imageUrl} />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://ep2023.europython.eu" />
         <meta property="twitter:title" content="EuroPython 2023 | July 17th-23rd 2023 | Prague, Czech Republic & Remote" />
         <meta property="twitter:description" content="" />
+        <meta property="twitter:image" content={imageUrl} />
       </Head>
 
       <div className="flex items-center justify-center content-center min-h-screen">
