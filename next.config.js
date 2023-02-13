19a35186369642d8f65ca9d8f5fa2a59fd469606
hypha <ramble.dalloway@gmail.com>
2023-02-13 14:46:55 +0000

    Revert "Add CFP Page Content"
    This reverts commit e4a470b43f82698cd730a00ce095e5a9c14294fe.

--- next.config.js
+++ next.config.js
@@ -35,6 +35,11 @@ const nextConfig = {
   async redirects() {
     return [
       {
+        source: "/cfp",
+        destination: "https://program.europython.eu/europython-2022/cfp",
+        permanent: true,
+      },
+      {
         source: "/sponsor-information",
         destination: "/sponsor/information",
         permanent: true,
