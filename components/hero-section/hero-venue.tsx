1788c2584b0b76f2d33e3cddb1855c4cf27ad372
Patrick Arminio <patrick.arminio@gmail.com>
2023-02-28 08:21:04 -0600

    Update images

--- components/hero-section/hero-venue.tsx
+++ components/hero-section/hero-venue.tsx
@@ -13,17 +13,16 @@ export const HeroVenue = ({
     <article className="lg:grid gap-12 grid-cols-[1fr_40rem]">
       <div className="relative hidden lg:block">
         <img
-          src="/img/venue.png"
+          src="/img/venue.jpg"
           className="absolute left-24 top-12 w-7/12"
           alt=""
         />
         <img src="/img/burst.png" className="absolute -left-1/2" alt="" />
         <img
           src="/img/photo.png"
-          className="absolute top-1/2 w-3/4 right-0 translate-y-16"
+          className="absolute top-1/2 w-3/4 translate-y-16"
           alt=""
         />
-        <img src="/img/map.png" className="absolute top-1/2 w-5/12" alt="" />
       </div>
       <div>
         <h2 className="mt-12 mb-8 font-bold text-5xl">{title}</h2>
