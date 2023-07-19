7d733a7ad7be716fcc8a6d336b276f8b5cee1fea
Patrick Arminio <patrick.arminio@gmail.com>
2023-07-19 10:00:39 +0200

    UA

--- lib/pretalx/utils/fetch.ts
+++ lib/pretalx/utils/fetch.ts
@@ -2,7 +2,7 @@ export const fetchWithToken: typeof fetch = async (url, options) => {
   const response = await fetch(url, {
     headers: {
       Authorization: `Token ${process.env.PRETALX_TOKEN}`,
-      "User-Agent": "europython.io4",
+      "User-Agent": "europython.io5",
     },
     ...options,
   });
