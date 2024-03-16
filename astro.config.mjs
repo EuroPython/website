13209a337d00c7fd987c0b47c740494d356297ee
Patrick Arminio <patrick.arminio@gmail.com>
2024-03-16 15:38:12 +0100

    Various fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,19 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+import remarkToc from 'remark-toc';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 
 import metaTags from "astro-meta-tags";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    remarkPlugins: [[remarkToc, { heading: "contents" }]],
+    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'append' }]],
+  },
+
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), react(), tailwind(), metaTags()]
 });
