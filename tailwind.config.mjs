9a896578578784ac92bd3a31ff79dec8a17e1c6d
Patrick Arminio <patrick.arminio@gmail.com>
2024-03-17 16:00:44 +0100

    one more font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -54,7 +54,7 @@ export default {
 			fontFamily: {
 				system: ["system-ui", "sans-serif"],
 				title: [
-					"Hanken Grotesk",
+					"Space Grotesk",
 					"-apple-system",
 					"BlinkMacSystemFont",
 					"Segoe UI",
