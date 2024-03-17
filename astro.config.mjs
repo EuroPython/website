f2255e13bc3473f52bc1c1d3d5be1ab84ab4e177
Patrick Arminio <patrick.arminio@gmail.com>
2024-03-17 12:59:11 +0100

    Fix URL and remove unused RSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import remarkToc from 'remark-toc';
-import rehypeSlug from 'rehype-slug';
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import remarkToc from "remark-toc";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import metaTags from "astro-meta-tags";
 
 import vercel from "@astrojs/vercel/static";
@@ -13,15 +13,26 @@ import vercel from "@astrojs/vercel/static";
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    remarkPlugins: [[remarkToc, {
-      heading: "contents"
-    }]],
-    rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, {
-      behavior: 'append'
-    }]]
+    remarkPlugins: [
+      [
+        remarkToc,
+        {
+          heading: "contents",
+        },
+      ],
+    ],
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+        },
+      ],
+    ],
   },
-  site: 'https://example.com',
+  site: "https://ep2024.europython.eu",
   integrations: [mdx(), sitemap(), react(), tailwind(), metaTags()],
   output: "static",
-  adapter: vercel()
+  adapter: vercel(),
 });
