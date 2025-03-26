34c2809114ef696e806a2855ed6a8a3011b1a019
Niko <nikoshell20@gmail.com>
2025-03-26 12:39:38 +0100

    Few tweaks to astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
+
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import { h } from "hastscript";
@@ -10,8 +13,21 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import metaTags from "astro-meta-tags";
 import pagefind from "astro-pagefind";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename); // @type-check enabled!
+
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    define: {
+      'process.env.VITE_BUILD_TIME': JSON.stringify(new Date().toISOString()),
+    },
+    resolve: {
+      alias: {
+        '$': path.resolve(__dirname, './src')
+      }
+    },
+  },
   markdown: {
     remarkPlugins: [
       [
@@ -46,4 +62,7 @@ export default defineConfig({
     pagefind(),
   ],
   output: "static",
+  build: {
+    minify: true
+  },
 });
