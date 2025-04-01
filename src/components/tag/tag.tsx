894c208c7c0983d8a9af1ebf5c41ceafd7e4a604
Niko <nikoshell20@gmail.com>
2025-04-01 14:53:54 +0200

    Code formatting.

--- src/components/tag/tag.tsx
+++ src/components/tag/tag.tsx
@@ -14,7 +14,7 @@ export const Tag = ({ children, className, href }: TagProps) => {
         "inline-block bg-secondary text-body px-4 py-2 rounded-xl font-bold",
         "hover:bg-primary hover:text-white transition-colors",
         "duration-200 ease-in-out cursor-pointer",
-        className
+        className,
       )}
       href={href}
     >
