2f2f63d14d166f623f9babaf85de6507d742bc40
Niko <nikoshell20@gmail.com>
2025-04-29 10:26:29 +0200

    Fix chromium args.

--- scripts/download_social.cjs
+++ scripts/download_social.cjs
@@ -3,6 +3,7 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch({
     executablePath: process.env.CHROME_BIN,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
   await page.goto("http://localhost:4321/media/social_media_cards");
