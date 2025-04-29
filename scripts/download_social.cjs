66947082c0e089cffb344bd88830515167f9b454
Niko <nikoshell20@gmail.com>
2025-04-29 08:40:29 +0200

    Add social media cards.

---
+++ scripts/download_social.cjs
@@ -0,0 +1,18 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({
+    executablePath: process.env.CHROME_BIN,
+  });
+  const page = await browser.newPage();
+  await page.goto("http://localhost:4321/social/");
+
+  const elements = await page.$$(".social");
+
+  for (let i = 0; i < elements.length; i++) {
+    const el = elements[i];
+    await el.screenshot({ path: `social-${i}.png` });
+  }
+
+  await browser.close();
+})();
