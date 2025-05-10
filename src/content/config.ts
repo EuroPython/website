67ecf2c8c57aea06c28b93c192b524406fd432e3
Niko <nikoshell20@gmail.com>
2025-05-10 12:18:32 +0200

    Update toc with active section.

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,7 +10,7 @@ const pages = defineCollection({
   schema: z.object({
     title: z.string(),
     subtitle: z.string(),
-    toc: z.boolean().optional().default(false),
+    toc: z.boolean().optional().default(true),
   }),
 });
 
