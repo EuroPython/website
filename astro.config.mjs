b5782fab1f5a8ce36a5f3d4f477b5a94c43e9153
Niko <nikoshell20@gmail.com>
2025-05-13 15:34:01 +0200

    Add 301 redirects.

--- astro.config.mjs
+++ astro.config.mjs
@@ -76,6 +76,7 @@ export default defineConfig({
   redirects: {
     "/planning/": "https://forms.gle/riw6CvML8ck94A4V9",
     "/reviewers/": "https://forms.gle/4GTJjwZ1nHBGetM18",
+    "/[code]": "/session/[...slug]",
   },
   integrations: [
     preload(),
