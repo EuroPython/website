3172a31da048c13ab919e00cc298b355bfc87e8a
Niko <nikoshell20@gmail.com>
2025-05-19 10:44:19 +0200

    Update component.

--- src/components/island/CodeHeart.svelte
+++ src/components/island/CodeHeart.svelte
@@ -75,7 +75,7 @@
   });
 </script>
 
-{#if !mini || isFavorite }
+{#if !mini}
   <div class="code-heart">
     <button
       class="heart-button"
@@ -93,6 +93,14 @@
           </svg>
       {/if}
     </button>
+  </div>
+{:else}
+  <div class="code-heart">
+      {#if isFavorite}
+        <svg xmlns="http://www.w3.org/2000/svg" class="heart-icon filled" viewBox="0 0 24 24" width="24" height="24">
+          <path fill="currentColor" d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z"/>
+        </svg>
+      {/if}
   </div>
 {/if}
 
