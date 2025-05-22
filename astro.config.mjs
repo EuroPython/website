baf21af3c2486d29735c70562035efb342e46a9c
Niko <nikoshell20@gmail.com>
2025-05-22 05:33:12 +0200

    Fix failing build process

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,9 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import metaTags from "astro-meta-tags";
 import pagefind from "astro-pagefind";
 import deleteUnusedImages from "astro-delete-unused-images";
-import preload from "astro-preload";
 import { execSync } from "node:child_process";
+
+import compress from "astro-compress";
 
 let gitVersion = String(process.env.GIT_VERSION ?? "").slice(0, 7);
 
@@ -24,6 +25,28 @@ if (!gitVersion) {
   } catch {
     gitVersion = "unknown";
   }
+}
+
+function dontDie() {
+  return {
+    name: "dont-die",
+    hooks: {
+      "astro:config:setup": () => {
+        process.on("uncaughtException", (error) => {
+          if (
+            error.message &&
+            error.message.includes("Failed to load remote image")
+          ) {
+            console.warn(
+              "[dont-die] Caught remote image error:",
+              error.message
+            );
+            return;
+          }
+        });
+      },
+    },
+  };
 }
 
 // https://astro.build/config
@@ -79,7 +102,6 @@ export default defineConfig({
     "/speaker/savannah-ostrowski": "/speaker/savannah-bailey",
   },
   integrations: [
-    preload(),
     mdx(),
     sitemap(),
     tailwind({
@@ -88,9 +110,10 @@ export default defineConfig({
     metaTags(),
     pagefind(),
     deleteUnusedImages(),
-    (await import("astro-compress")).default({
+    compress({
       SVG: false,
     }),
+    dontDie(),
   ],
   output: "static",
   build: {
