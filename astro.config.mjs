8fc155f5d6bf1ec7204399cf20c1b0bc7a1cc9a5
Nikoś <nikoshell20@gmail.com>
2025-05-22 15:19:50 +0200

    Add missing redirects. (#1260)

--- astro.config.mjs
+++ astro.config.mjs
@@ -99,9 +99,12 @@ export default defineConfig({
     "/planning/": "https://forms.gle/riw6CvML8ck94A4V9",
     "/reviewers/": "https://forms.gle/4GTJjwZ1nHBGetM18",
     "/speaker/savannah-ostrowski": "/speaker/savannah-bailey",
-    "/rust-summit": "/session//rust-summit",
+    "/rust-summit": "/session/rust-summit",
     "/c-api-summit": "/session/c-api-summit",
     "/wasm-summit": "/session/webassembly-summit",
+    "/programme/rust-summit": "/session/rust-summit",
+    "/programme/c-api-summit": "/session/c-api-summit",
+    "/programme/wasm-summit": "/session/webassembly-summit",
   },
   integrations: [
     mdx(),
