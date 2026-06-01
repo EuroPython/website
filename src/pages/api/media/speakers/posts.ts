c061ce17c35ca46cead97e908f75d696fab1c823
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2026-06-01 22:56:55 +0300

    Remove period after talk title
    Followed by lowercase 'talk'

--- src/pages/api/media/speakers/posts.ts
+++ src/pages/api/media/speakers/posts.ts
@@ -66,21 +66,21 @@ export const GET: APIRoute = async () => {
 
     x: ({ name, handle, talkTitle, talkUrl }) =>
       handle
-        ? `Join ${name} (${handle}) at EuroPython for "${talkTitle}". Talk: ${talkUrl}`
-        : `Join ${name} at EuroPython for "${talkTitle}". Talk: ${talkUrl}`,
+        ? `Join ${name} (${handle}) at EuroPython for "${talkTitle}" talk: ${talkUrl}`
+        : `Join ${name} at EuroPython for "${talkTitle}" talk: ${talkUrl}`,
 
     linkedin: ({ name, talkTitle }) =>
       `Join ${name} at EuroPython for "${talkTitle}".`,
 
     bsky: ({ name, handle, talkTitle, talkUrl }) =>
       handle
-        ? `Join ${name} (${handle}) at EuroPython for "${talkTitle}". Talk: ${talkUrl}`
-        : `Join ${name} at EuroPython for "${talkTitle}". Talk: ${talkUrl}`,
+        ? `Join ${name} (${handle}) at EuroPython for "${talkTitle}" talk: ${talkUrl}`
+        : `Join ${name} at EuroPython for "${talkTitle}" talk: ${talkUrl}`,
 
     fosstodon: ({ name, handle, talkTitle, talkUrl }) =>
       handle
-        ? `Join ${name} (${handle}) at EuroPython for "${talkTitle}". talk: ${talkUrl}`
-        : `Join ${name} at EuroPython for "${talkTitle}". Talk: ${talkUrl}`,
+        ? `Join ${name} (${handle}) at EuroPython for "${talkTitle}" talk: ${talkUrl}`
+        : `Join ${name} at EuroPython for "${talkTitle}" talk: ${talkUrl}`,
   };
 
   const trimToLimit = (text: string, limit: number) =>
